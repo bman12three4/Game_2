a866e5b3f08ca3b09c58c0cf8435b292090ca73d
Byron Lathi <byron.lathi@student.lovett.org>
2016-11-30 14:17:22 -0500

    Changed it again to test

--- Game_2/src/developers/Game2.java
+++ Game_2/src/developers/Game2.java
@@ -3,7 +3,7 @@ package developers;
 public class Game2 {
 
 	public static void main(String[] args) {
-		System.out.println("blah");
+		System.out.println("Yay at least it works for one person");
 	}
 
 }
