dfbd2f8c6eb12d8d1ff5733f3cb8d22c968162a7
Byron Lathi <byron.lathi@student.lovett.org>
2016-11-30 21:16:47 -0500

    Added init check to main.

--- Game_2/src/developers/Game2.java
+++ Game_2/src/developers/Game2.java
@@ -4,9 +4,19 @@ import java.io.IOException;
 
 public class Game2 {
 
+	private static boolean init = false;
+	
+	public static boolean isInit(){
+		return init;
+	}
+
 	public static void main(String[] args) throws IOException {
-		WindowRunner.init("Game_2");
-		Player player = new Player("img/dude.png");
+		if (!isInit()) {
+			WindowRunner.init("Game_2");
+			Player player = new Player("img/dude.png");
+			init = true;
+		}
+		
 	}
 
 }
