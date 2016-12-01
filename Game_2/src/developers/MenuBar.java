decb83786904476a2a07736b72f1ff5689a0bfba
Byron Lathi <byron.lathi@student.lovett.org>
2016-11-30 22:19:46 -0500

    Added ActionListener to help menu

--- Game_2/src/developers/MenuBar.java
+++ Game_2/src/developers/MenuBar.java
@@ -1,15 +1,19 @@
 package developers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class MenuBar extends JMenuBar{
-	
+public class MenuBar extends JMenuBar implements ActionListener {
+
 	private static final long serialVersionUID = 1L;
-	
+
+	public static final int HELP_ID = 1001;
+
 	JMenu menu, submenu;
 	JMenuItem menuItem;
 
@@ -17,9 +21,18 @@ public class MenuBar extends JMenuBar{
 		menu = new JMenu("Help");
 		menu.setMnemonic(KeyEvent.VK_H);
 		add(menu);
-		
+
 		menuItem = new JMenuItem("How to play");
+		menuItem.addActionListener(this);
 		menu.add(menuItem);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getID() == HELP_ID){
+			
+		}	
+
 	}
 
 }
