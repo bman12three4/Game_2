932e242f8333b6ac070b8e5bd4a18ea92667620f
Byron Lathi <byron.lathi@student.lovett.org>
2016-12-02 12:56:53 -0500

    Added Ladder Object

---
+++ Game_2/src/developers/elements/Ladder.java
@@ -0,0 +1,121 @@
+package developers.elements;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+
+public class Ladder extends JComponent{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Dimension dim;
+	private int xPos, yPos;
+	private BufferedImage img;
+	
+	ClassLoader cl = this.getClass().getClassLoader();
+
+	public Ladder() throws IOException {
+		this.dim = null;
+		this.img = null;
+	}
+	
+	public Ladder(int x, int y) throws IOException{
+		this.xPos = x;
+		this.yPos = y;
+		img = ImageIO.read(cl.getResource("img/largeplatform.png"));
+	}
+	
+	/**
+	 * @return the dim
+	 */
+	public Dimension getDim() {
+		return dim;
+	}
+
+	/**
+	 * @param dim the dim to set
+	 */
+	public void setDim(Dimension dim) {
+		this.dim = dim;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return xPos;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.xPos = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return yPos;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.yPos = y;
+	}
+
+	/**
+	 * @return the img
+	 */
+	public BufferedImage getImg() {
+		return img;
+	}
+
+	/**
+	 * @param img the img to set
+	 */
+	public void setImg(BufferedImage img) {
+		this.img = img;
+	}
+
+	/**
+	 * @return the cl
+	 */
+	public ClassLoader getCl() {
+		return cl;
+	}
+
+	/**
+	 * @param cl the cl to set
+	 */
+	public void setCl(ClassLoader cl) {
+		this.cl = cl;
+	}
+
+	public Ladder(int x, int y, String img, Dimension dim) throws IOException{
+		this.xPos =x;
+		this.yPos = y;
+		this.dim = dim;
+		this.img = ImageIO.read(cl.getResource(img));
+	}
+	
+	public Ladder(int x, int y, BufferedImage img, Dimension dim) throws IOException{
+		this.xPos =x;
+		this.yPos = y;
+		this.dim = dim;
+		this.img = img;
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(img, xPos, yPos, this);
+	}
+
+}
