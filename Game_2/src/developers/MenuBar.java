3c1f859338b373966a2840efe45d23c41f823f85
Byron Lathi <byron.lathi@student.lovett.org>
2016-12-05 12:04:25 -0500

    Tried to add help menu (broken)

--- Game_2/src/developers/MenuBar.java
+++ Game_2/src/developers/MenuBar.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -21,6 +22,8 @@ public class MenuBar extends JMenuBar implements ActionListener {
 
 	JMenu menu, submenu;
 	JMenuItem menuItem;
+	
+	JLabel help;
 
 	public MenuBar() {
 		menu = new JMenu("Help");
@@ -30,12 +33,15 @@ public class MenuBar extends JMenuBar implements ActionListener {
 		menuItem = new JMenuItem("How to play");
 		menuItem.addActionListener(this);
 		menu.add(menuItem);
+		help = new JLabel("Its pretty easy");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getID() == HELP_ID){
-			
+			System.out.println("Lol hes asking for help what a noob");
+			WindowRunner.getPanel().add(help);
+			help.setVisible(true);
 		}	
 
 	}
